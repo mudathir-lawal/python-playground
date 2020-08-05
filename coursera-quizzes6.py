d8e94bce28c5129c259eb5a773db34822627ae27
Mudathir Lawal <nigercode@gmail.com>
2020-08-05 17:48:50 +0100

    sample func to return a tuple

--- coursera-quizzes6.py
+++ coursera-quizzes6.py
@@ -32,3 +32,17 @@ Ken is 30 years old and works as Chef
 Pat is 35 years old and works as Lawyer
 Amanda is 25 years old and works as Engineer
 """
+
+def group_list(group, users):
+  members = ""
+  for idx, user in enumerate(users):
+    if idx < len(users) - 1:
+      members += (' {},'.format(user))
+    else:
+      members += (' {}'.format(user))   
+  return '{}: {}'.format(group, members)
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
--- test.py
+++ test.py
@@ -1,15 +1,29 @@
+def group_list(group, users):
+  members = ""
+  for idx, user in enumerate(users):
+    if idx < len(users) - 1:
+      members += (' {},'.format(user))
+    else:
+      members += (' {}'.format(user))   
+  return '{}: {}'.format(group, members)
 
-def pig_latin(text):
-  say = ""
-  # Separate the text into words
-  words = text.split()
-  print(words)
-  for word in words:
-    # Create the pig latin word and add it to the list
-    words.append('{}{}'.format(text[0], 'ay'))
-    # Turn the list back into a phrase
-    words = say.join(words)
-  return words
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
+
+# UNDER CONSTRUCTION:
+# def pig_latin(text):
+#   say = ""
+#   # Separate the text into words
+#   words = text.split()
+#   print(words)
+#   for word in words:
+#     # Create the pig latin word and add it to the list
+#     words.append('{}{}'.format(text[0], 'ay'))
+#     # Turn the list back into a phrase
+#     words = say.join(words)
+#   return words
 		
-print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
-print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+# print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+# print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
