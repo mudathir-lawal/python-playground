1ae113d1d5f3a1303348d04631dc96a0d079722e
Mudathir Lawal <nigercode@gmail.com>
2020-08-06 16:36:31 +0100

    accessing values of nested a dict

--- coursera-quizzes7.py
+++ coursera-quizzes7.py
@@ -2,4 +2,9 @@ cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
 for amphibian, organ in cool_beasts.items():
     print("{} have {}".format(amphibian, organ))
 
-    
+wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
+for clothing, colour_list in wardrobe.items():
+	for colour in colour_list:
+		print("{} {}".format(colour, clothing))
+        
+            
