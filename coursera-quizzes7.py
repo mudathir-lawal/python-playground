d17d69ebcf46966227035877d1e1168edbe284af
Mudathir Lawal <nigercode@gmail.com>
2020-08-06 20:02:35 +0100

    implement dictionary update

--- coursera-quizzes7.py
+++ coursera-quizzes7.py
@@ -43,3 +43,17 @@ print(count_letters("Math is fun! 2+2=4"))
 print(count_letters("This is a sentence."))
 # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
 
+
+####
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  guests1.update(guests2)
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+print(combine_guests(Rorys_guests, Taylors_guests))
+print(Rorys_guests)
+######
+
+
