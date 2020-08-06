a24635338e7aa05deb7d6d04da7c14d4ee2b0cb7
Mudathir Lawal <nigercode@gmail.com>
2020-08-06 12:20:41 +0100

    add dictionary code

--- coursera-quizzes6.py
+++ coursera-quizzes6.py
@@ -98,3 +98,17 @@ print('\n')
 print(words)
 print(type(words))
 
+####
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+___ # Epilogue starts on page 39
+___ # Chapter 3 now starts on page 24
+___ # What are the current contents of the dictionary?
+___ # Is there a Chapter 5?
+
+toc["Epilogue"] = 39
+toc["Chapter 3"] = 24
+print(toc)
+if "Chapter 5" in toc:
+    print(True)
+else:
+    print(False)
