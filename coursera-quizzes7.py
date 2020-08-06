474e316d4133e92554afde7dd5a24f39c5066c68
Mudathir Lawal <nigercode@gmail.com>
2020-08-06 19:35:20 +0100

    implement func to count the chars in a string

--- coursera-quizzes7.py
+++ coursera-quizzes7.py
@@ -6,5 +6,40 @@ wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
 for clothing, colour_list in wardrobe.items():
 	for colour in colour_list:
 		print("{} {}".format(colour, clothing))
-        
-            
+
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+host_addresses.keys()
+colors = ["red", "white", "blue"]
+colors.insert(2, "yellow")
+print(colors)
+
+animal = "Hippopotamus"
+print(animal[3:6])
+print(animal[-5])
+print(animal[10:])
+
+
+def count_letters(text):
+  result = {}
+  text = text.lower()
+  # Go through each letter in the text
+  for letter in text:
+    # Check if the letter needs to be counted or not
+    if letter.isalpha():
+      if letter not in result:
+        result[letter] = 0
+      # Add or increment the value in the dictionary
+      result[letter] += 1 
+    else:
+      continue  
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
+
--- test.py
+++ test.py
@@ -18,18 +18,15 @@ print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay
 
 
 
+def group_list(group, users):
+  members = ""
+  for user in users:
+      members += (' {},'.format(user))  
+  return '{}: {}'.format(group, members.__(len(users) - 1))
 
-
-
-# def group_list(group, users):
-#   members = ""
-#   for user in users:
-#       members += (' {},'.format(user))  
-#   return '{}: {}'.format(group, members.__(len(users) - 1))
-
-# print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
-# print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
-# print(group_list("Users", "")) # Should be "Users:"
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
 
 
 # UNDER CONSTRUCTION:
