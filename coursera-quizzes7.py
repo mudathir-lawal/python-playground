7a9d0e76b66eabc1431ff2eb67916bcf57bc8178
Mudathir Lawal <nigercode@gmail.com>
2020-08-07 04:18:27 +0100

    implement bug fix

--- coursera-quizzes7.py
+++ coursera-quizzes7.py
@@ -104,12 +104,13 @@ def format_address(address_string):
   for address in address_list:
     # Determine if the address part is the
     # house number or part of the street name
-    if address.isalpha() or address.isalnum():
+    if address.isnumeric():
+      house_number += int(address)
+    # elif address.isalpha() or address.isalnum():
+    else:
       street_name += ' {}'.format( address )
     # Does anything else need to be done 
     # before returning the result?
-    else:
-      house_number += address 
   # Return the formatted string  
   return "house number {} on street named {}".format(house_number, street_name)
 
@@ -121,7 +122,6 @@ print(format_address("1001 1st Ave"))
 
 print(format_address("55 North Center Drive"))
 # Should print "house number 55 on street named North Center Drive"
-
 #####
 
 
