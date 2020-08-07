1cf42d25214bdfb337787f7a698de92dcf7ec8ab
Mudathir Lawal <nigercode@gmail.com>
2020-08-07 08:07:03 +0100

    add combine_dict() & resize files

--- coursera-quizzes8.py
+++ coursera-quizzes8.py
@@ -33,3 +33,65 @@ print(color_translator("red")) # Should be #ff0000
 print(color_translator("black")) # Should be unknown
 print(color_translator("green")) # Should be #00ff00
 print(color_translator("")) # Should be unknown
+######
+
+def format_address(address_string):
+  # Declare variables
+  address_string.strip()
+  house_number = 0
+  street_name = ''
+  # Separate the address string into parts
+  address_list = address_string.split()
+  # Traverse through the address parts
+  for address in address_list:
+    # Determine if the address part is the
+    # house number or part of the street name
+    if address.isnumeric():
+      house_number += int(address.strip())
+    elif address.isalpha() or address.isalnum():
+      street_name += ' {}'.format( address.strip() )
+    # Does anything else need to be done 
+    # before returning the result?
+    else:
+      continue
+    street_name = street_name.strip()
+  # Return the formatted string  
+  return "house number {} on street named {}".format(house_number, street_name)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
+######
+
+def combine_lists(list1, list2):
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+  list2.reverse()
+  list1.extend( list2 )
+  # ordered_list2 = [ (list2[(len(list2)- 1)- i]) for i, student in enumerate(list2) ]
+  # list1 = list1 + ordered_list2
+  # for stud in ordered_list2:
+  #   list1.append( stud ) 
+  return list1
+
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+print(combine_lists(Jamies_list, Drews_list))
+####################
+def combine_guests(guests1, guests2):
+  # Combine both dictionaries into one, with each key listed 
+  # only once, and the value from guests1 taking precedence
+  guests2.update(guests1)
+  return guests2
+
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
+######
+
