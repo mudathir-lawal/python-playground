dae55c12e7759feeb3e6a9816baf777ecd3eba62
Mudathir Lawal <nigercode@gmail.com>
2020-08-07 07:54:02 +0100

    add another version of dict func

--- coursera-quizzes7.py
+++ coursera-quizzes7.py
@@ -137,6 +137,38 @@ Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
 print(combine_lists(Jamies_list, Drews_list))
 #############
 
+def format_address(address_string):
+  # Declare variables
+  address_string.strip()
+  house_number = 0
+  street_name = ''
+  # Separate the address string into parts
+  address_list = address_string.split()
+  # Traverse through the address parts
+  for address in address_list:
+    # Determine if the address part is the
+    # house number or part of the street name
+    if address.isnumeric():
+      house_number += int(address.strip())
+    elif address.isalpha() or address.isalnum():
+      street_name += ' {}'.format( address.strip() )
+    # Does anything else need to be done 
+    # before returning the result?
+    else:
+      continue
+    street_name = street_name.strip()
+  # Return the formatted string  
+  return "house number {} on street named {}".format(house_number, street_name)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
+######
 
 
 
