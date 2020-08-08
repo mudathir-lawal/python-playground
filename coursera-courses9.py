1eadf61c5cbb6d2fd11b5eeb4dbdaf7b37c63338
Mudathir Lawal <nigercode@gmail.com>
2020-08-08 23:59:11 +0100

    add solution to assessment Q5

--- coursera-courses9.py
+++ coursera-courses9.py
@@ -28,5 +28,14 @@ print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
 print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 ########
 
+def car_listing(car_prices):
+  result = ""
+  for car_name, car_price in car_prices.items():
+    result += "{} costs {} dollars".format(car_name, car_price) + "\n"
+  return result
+
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
+############
+
 
 
