eddc50de7fcc470477b3b8b3b94251e67a0dd1c0
Mudathir Lawal <nigercode@gmail.com>
2020-08-08 23:57:33 +0100

    add sqaures() func

--- coursera-courses9.py
+++ coursera-courses9.py
@@ -20,3 +20,13 @@ print(highlight_word("Shhh, don't be so loud!", "loud"))
 print(highlight_word("Automating with Python is fun", "fun"))
 
 #######
+def squares(start, end):
+	return [ num * num for num in range(start, end + 1)]
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+########
+
+
+
