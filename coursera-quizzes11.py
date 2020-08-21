9e9d7b0eeb07a9b526a20b6f057a422ae4ebdb9a
Mudathir Lawal <nigercode@gmail.com>
2020-08-21 08:02:51 +0100

    Update coursera-quizzes11.py

--- coursera-quizzes11.py
+++ coursera-quizzes11.py
@@ -19,3 +19,18 @@ def count_letters(text):
 no_of_chars = count_letters("Shokolokobangoshe")
 print(no_of_chars)
 print(no_of_chars.values())
+
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "Red"
+
+violet = Flower()
+violet.color = "Purple"
+
+this_pun_is_for_you = "This pun is for you."
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you) 
