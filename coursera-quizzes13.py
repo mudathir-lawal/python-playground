d8b40e114126e2fab547d57e2af39a8a3bd5b17c
Mudathir Lawal <nigercode@gmail.com>
2020-08-21 09:50:07 +0100

    create describe_furniture func

---
+++ coursera-quizzes13.py
@@ -0,0 +1,20 @@
+class Furniture:
+	color = ""
+	material = ""
+
+table = Furniture()
+table.color = "brown"
+table.material = "wood"
+#____
+
+couch = Furniture()
+couch.color = "red"
+couch.material = "leather"
+
+def describe_furniture(piece):
+	return ("This piece of furniture is made of {} {}".format(piece.color, piece.material))
+
+print(describe_furniture(table)) 
+# Should be "This piece of furniture is made of brown wood"
+print(describe_furniture(couch)) 
+# Should be "This piece of furniture is made of red leather"
