b24a65fc5d5fcee57ded493d40ae931ec6edd6e4
Mudathir Lawal <nigercode@gmail.com>
2020-08-23 03:32:03 +0100

    Add snake class

--- coursera-quizzes14.py
+++ coursera-quizzes14.py
@@ -28,3 +28,19 @@ print(Turtle.category)
 
 class Snake(Animal):
     category = "reptile"
+
+class Zoo:
+    def __init__(self):
+        self.current_animals = {}
+    
+    def add_animal(self, animal):
+        self.current_animals[animal.name] = animal.category
+    
+    def total_of_category(self, category):
+        result = 0
+        for animal in self.current_animals.values():
+            if animal == category:
+                result += 1
+        return result
+
+zoo = Zoo()
