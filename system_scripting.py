60dcbd8512f273698b7232b810b86ed391103e4f
Mudathir Lawal <nigercode@gmail.com>
2020-08-24 13:03:14 +0100

    Add alternative implementation for avg_load() method

--- system_scripting.py
+++ system_scripting.py
@@ -11,11 +11,13 @@ class Server:
         connection_load = random.random()*10+1
         # Add the connection to the dictionary with the calculated load
         self.connections[connection_id] = connection_load
+        self.load()
 
     def close_connection(self, connection_id):
         """Closes a connection on this server."""
         # Remove the connection from the dictionary
         del self.connections[connection_id]
+        self.load()
 
     def load(self):
         """Calculates the current load for all connections."""
@@ -33,11 +35,11 @@ class Server:
 
 server = Server()
 server.add_connection("192.168.1.1")
+
 print(server.load())
 
 server.close_connection("192.168.1.1")
 print(server.load())
-
 
 #Begin Portion 2#
 class LoadBalancing:
@@ -52,31 +54,47 @@ class LoadBalancing:
         # Add the connection to the dictionary with the selected server
         self.connections[connection_id] = server
         # Add the connection to the server
-        server.add_connection(connection_id)        
+        server.add_connection(connection_id)
+        self.ensure_availability()
+        server.load()
 
     def close_connection(self, connection_id):
         """Closes the connection on the the server corresponding to connection_id."""
         # Find out the right server
-        for connection, server in enumerate(self.connections.items()):
+        for connection, server in enumerate(self.servers):
             if connection == connection_id:
                 # Close the connection on the server
-                server.close_connection[connection_id]
-                # Remove the connection from the load balancer
-                del self.connections[connection]
+                server.close_connection(connection_id)
                 server.load()
+                # Remove the connection from the load balancer
+                self.connections[connection_id] = 0
+                self.servers.load()
+                del self.connections[connection_id]
 
     def avg_load(self):
         """Calculates the average load of all servers"""
         # Sum the load of each server and divide by the amount of servers
-        avg = 0
-        for machine in self.servers:
-            avg += machine.load()
-        avg = avg / len(self.servers)   
-        return avg
+        average = 0
+        load_sum = 0
+        for nos, server in enumerate(self.servers):
+            load_sum += server.load()
+            nos += 1
+        average = load_sum / nos    
+        return average
+
+    # ALTERNATIVE IMPLEMENTATION:
+    # def avg_load(self):
+    #     """Calculates the average load of all servers"""
+    #     # Sum the load of each server and divide by the amount of servers
+    #     avg = 0
+    #     for machine in self.servers:
+    #         avg += machine.load()
+    #     avg = avg / len(self.servers)    
+    #     return avg    
 
     def ensure_availability(self):
         """If the average load is higher than 50, spin up a new server"""
-        if avg_load() > 50:
+        while self.avg_load() > 50:
             self.servers.append(Server())
 
     def __str__(self):
@@ -84,3 +102,20 @@ class LoadBalancing:
         loads = [str(server) for server in self.servers]
         return "[{}]".format(",".join(loads))
 #End Portion 2#
+
+l = LoadBalancing()
+l.add_connection("fdca:83d2::f20d")
+print(l.avg_load())
+
+l.servers.append(Server())
+print(l.avg_load())
+
+l.close_connection("fdca:83d2::f20d")
+print(l.avg_load())
+
+for connection in range(20):
+    l.add_connection(connection)
+print(l)
+
+print(l.avg_load())
+
